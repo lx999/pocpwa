5f046b9f97d1760ebdd9dc94c7eab35460d858eb
FR01_AWL\A689415 <xiang.2.li@worldline.com>
2018-02-26 11:52:43 +0100

    11h52 26 02

---
+++ static/js/register_sw.js
@@ -0,0 +1,38 @@
+const webpush = require('web-push');
+//VAPID keys should only be generated only once.
+const vapidKeys = webpush.generateVAPIDKeys();
+console.log(vapidKeys.publicKey, vapidKeys.privateKey);
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js')
+    .then(reg => {
+      // registration worked
+      console.log('[Service Worker] Registration succeeded. Scope is ' + reg.scope);
+      if ('Notification' in window) {
+        console.log('Notification permission default status:', Notification.permission);
+        Notification.requestPermission(function (status) {
+          console.log('Notification permission status:', status);
+          displayNotification();
+        });
+      }
+    }).catch(error => {
+      // registration failed
+      console.log('[Service Worker] Registration failed with ' + error);
+    });
+}
+
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(reg => {
+      var options = {
+        icon: '/pocpwa/static/img/icons/android-chrome-200x200_LFP.png',
+        body: '歡迎加入 Angular 社群',
+        data: {
+          link: 'https://forum.angular.tw/'
+        }
+      };
+      reg.showNotification('Angular User Group Taiwan', options);
+      console.log('displayNotification');
+    });
+  }
+}
---
+++ static/js/sw.js
@@ -0,0 +1,26 @@
+self.addEventListener('notificationclick', event => {
+  const notification = event.notification;
+  const action = event.action;
+  const link = notification.data.link;
+  if (action !== 'close') {
+    if (link) {
+      clients.openWindow(link);
+    }
+  }
+  notification.close();
+  console.log('notificationclick action is', action);
+});
+
+self.addEventListener('push', event => {
+  console.log('[Service Worker] Push Received.');
+  let title = 'Server Push';
+  let options = {
+    body: 'push TEST',
+    icon: '/pocpwa/static/img/icons/android-chrome-200x200_LFP.png'
+  };
+  if (event.data) {
+    options = event.data.json();
+    title = options.title;
+  }
+  event.waitUntil(self.registration.showNotification(title, options));
+});
