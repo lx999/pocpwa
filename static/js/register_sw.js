0c3beae7aaac86971819aa199fc7e7e82b43e5a0
FR01_AWL\A689415 <xiang.2.li@worldline.com>
2018-02-26 12:01:28 +0100

    12h00 26 02

--- static/js/register_sw.js
+++ static/js/register_sw.js
@@ -1,8 +1,3 @@
-const webpush = require('web-push');
-//VAPID keys should only be generated only once.
-const vapidKeys = webpush.generateVAPIDKeys();
-console.log(vapidKeys.publicKey, vapidKeys.privateKey);
-
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js')
     .then(reg => {
