9058c4c775e21ebf94752f65570ff9273d5de954
FR01_AWL\A689415 <xiang.2.li@worldline.com>
2018-02-26 13:54:52 +0100

    13h54 26 02

---
+++ static/js/push.ts
@@ -0,0 +1,28 @@
+import * as webpush from 'web-push';
+const vapidKeys = {
+  publicKey: 'BPkIUOIylNfWjC9MQ3_8oVx0MsaryiEaak1WyYWyqWp1-FuyQitttiMkdjvACkoEds94crwhyRIyVTyc2tVYICI',
+  privateKey: '5ZwdOLmdJEmdsYcp-ERUtmMg328EKq7jMGSAn3nSBgM'
+};
+const webPush = webpush;
+webPush.setVapidDetails(
+  'mailto:denhuang@gmail.com',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
+const options = {
+  icon: '/pocpwa/static/img/icons/android-chrome-200x200_LFP.png',
+  body: 'Angular 測試工作坊 9月23日(六)',
+  data: {
+          link: 'https://forum.angular.tw/'
+        },
+  requireInteraction: true
+};
+const subscription = {
+  endpoint: 'https://fcm.googleapis.com/fcm/send/fwNbCkZtyr0:APA91bF-tttRSH0KBHuZ3lGehkd7kcNzWOfAVTKeXp4cYUURgq2bEkTkCtLQAvrzDZ7q_N7on0ved-Ss9SGLRYGm61D2rkmPe2R2EUnLn7s1y7Fwrjts2I-qM94SQINyJA4VBV5spTdy',
+  expirationTime: null,
+  keys: {
+    p256dh: 'BKsb5IumFOgJMBgk15Ce-oU123T5EuPFo3goBbRhwZarYlUfGC-I7JTKSee2TpmjG3HNZQcwirLYz2Fx9b_eZrU',
+    auth: 'I4qBuGIBj1pUVbT4sQnHEg=='
+  }
+};
+webPush.sendNotification(subscription, JSON.stringify(options));
--- static/js/register_sw.js
+++ static/js/register_sw.js
@@ -7,7 +7,7 @@ if ('serviceWorker' in navigator) {
         console.log('Notification permission default status:', Notification.permission);
         Notification.requestPermission(function (status) {
           console.log('Notification permission status:', status);
-          displayNotification();
+          // displayNotification();
         });
       }
     }).catch(error => {
@@ -30,4 +30,37 @@ function displayNotification() {
       console.log('displayNotification');
     });
   }
+}
+
+
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+const applicationServerPublicKey = `BKsb5IumFOgJMBgk15Ce-oU123T5EuPFo3goBbRhwZarYlUfGC-I7JTKSee2TpmjG3HNZQcwirLYz2Fx9b_eZrU`;
+
+function subscribeUser(swRegistration) {
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  swRegistration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: applicationServerKey
+    })
+    .then(subscription => {
+      console.log('User is subscribed');
+      console.log(JSON.stringify(subscription));
+    })
+    .catch(err => {
+      console.log('Failed to subscribe the user: ', err);
+    });
 }
