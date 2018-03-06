c69f5e955779703d9e95aa2e4f5e82af68ebf495
FR01_AWL\A689415 <xiang.2.li@worldline.com>
2018-03-06 15:08:11 +0100

    15h08 06 03

--- static/js/sw.js
+++ static/js/sw.js
@@ -8,16 +8,41 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
   console.log('Activated', event);
 });
-
-self.addEventListener('push', function(event) {
-  console.log('Push message', event);
-
-  var title = 'Le push de test :)';
-
-  event.waitUntil(
-    self.registration.showNotification(title, {
-     body: 'Bravo tu l\'as reçu',
-     icon: 'static/img/icons/faviconLFP.ico',
-     tag: 'my-tag'
-   }));
+self.addEventListener('push', event => {
+  console.log('[Service Worker] Push Received.');
+  let title = 'Server Push';
+  let options = {
+    body: 'push TEST',
+    icon: 'static/img/icons/faviconLFP.ico'
+  };
+  if (event.data) {
+    options = event.data.json();
+    title = options.title;
+  }
+  event.waitUntil(self.registration.showNotification(title, options));
 });
+// self.addEventListener('push', function(event) {
+//   console.log('Push message', event);
+
+//   var title = 'Le push de test :)';
+
+//   event.waitUntil(
+//     self.registration.showNotification(title, {
+//      body: 'Bravo tu l\'as reçu',
+//      icon: 'static/img/icons/faviconLFP.ico',
+//      tag: 'my-tag'
+//    }));
+// });
+
+self.addEventListener('notificationclick', event => {
+  const notification = event.notification;
+  const action = event.action;
+  const link = notification.data.link;
+  if (action !== 'close') {
+    if (link) {
+      clients.openWindow(link);
+    }
+  }
+  notification.close();
+  console.log('notificationclick action is', action);
+}) 
