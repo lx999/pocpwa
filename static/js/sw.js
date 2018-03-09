79e7ab46cc1ca06c2955e3dc18665617d97839b8
FR01_AWL\A689415 <xiang.2.li@worldline.com>
2018-03-09 10:13:43 +0100

    10h13 09 03

--- static/js/sw.js
+++ static/js/sw.js
@@ -8,34 +8,35 @@
 // self.addEventListener('activate', function(event) {
 //   console.log('Activated', event);
 // });
-
-self.addEventListener('push', function(event) {
-  console.log('[Service Worker] Push Received.');
-  console.log(event);
-  let title = 'Server Push';
-  let options = {
-    body: 'push TEST',
-    icon: 'https://lx999.github.io/pocpwa/static/img/icons/favicon.ico',
-    data: {
-        link: 'https://lx999.github.io/pocpwa/#/'
-      }
-  };
-  if (event.data) {
-    options = event.data.json();
-    title = options.title;
-  }
-  event.waitUntil(self.registration.showNotification(title, options));
-});
-
-self.addEventListener('notificationclick', event => {
-  const notification = event.notification;
-  const action = event.action;
-  const link = notification.data.link;
-  if (action !== 'close') {
-    if (link) {
-      clients.openWindow(link);
+export default {
+  self.addEventListener('push', function(event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(event);
+    let title = 'Server Push';
+    let options = {
+      body: 'push TEST',
+      icon: 'https://lx999.github.io/pocpwa/static/img/icons/favicon.ico',
+      data: {
+          link: 'https://lx999.github.io/pocpwa/#/'
+        }
+    };
+    if (event.data) {
+      options = event.data.json();
+      title = options.title;
     }
-  }
-  notification.close();
-  console.log('notificationclick action is', action);
-}) 
+    event.waitUntil(self.registration.showNotification(title, options));
+  });
+
+  self.addEventListener('notificationclick', event => {
+    const notification = event.notification;
+    const action = event.action;
+    const link = notification.data.link;
+    if (action !== 'close') {
+      if (link) {
+        clients.openWindow(link);
+      }
+    }
+    notification.close();
+    console.log('notificationclick action is', action);
+  })   
+}
