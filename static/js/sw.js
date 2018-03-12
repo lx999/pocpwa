f12235f3fb6b3053d18de7da046271aa56a721a2
FR01_AWL\A689415 <xiang.2.li@worldline.com>
2018-03-12 14:19:41 +0100

    14h19 12 03

--- static/js/sw.js
+++ static/js/sw.js
@@ -8,35 +8,35 @@
 // self.addEventListener('activate', function(event) {
 //   console.log('Activated', event);
 // });
-export default {
-  self.addEventListener('push', function(event) {
-    console.log('[Service Worker] Push Received.');
-    console.log(event);
-    let title = 'Server Push';
-    let options = {
-      body: 'push TEST',
-      icon: 'https://lx999.github.io/pocpwa/static/img/icons/favicon.ico',
-      data: {
-          link: 'https://lx999.github.io/pocpwa/#/'
-        }
-    };
-    if (event.data) {
-      options = event.data.json();
-      title = options.title;
-    }
-    event.waitUntil(self.registration.showNotification(title, options));
-  });
 
-  self.addEventListener('notificationclick', event => {
-    const notification = event.notification;
-    const action = event.action;
-    const link = notification.data.link;
-    if (action !== 'close') {
-      if (link) {
-        clients.openWindow(link);
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(event);
+  let title = 'Server Push';
+  let options = {
+    body: 'push TEST',
+    icon: 'https://lx999.github.io/pocpwa/static/img/icons/favicon.ico',
+    data: {
+        link: 'https://lx999.github.io/pocpwa/#/'
       }
+  };
+  if (event.data) {
+    options = event.data.json();
+    title = options.title;
+  }
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', event => {
+  const notification = event.notification;
+  const action = event.action;
+  const link = notification.data.link;
+  if (action !== 'close') {
+    if (link) {
+      clients.openWindow(link);
     }
-    notification.close();
-    console.log('notificationclick action is', action);
-  })   
-}
+  }
+  notification.close();
+  console.log('notificationclick action is', action);
+})   
+
