00f94c9d8196d5d13d96c69b99bcf6334f09ea08
FR01_AWL\A689415 <xiang.2.li@worldline.com>
2018-03-13 13:40:28 +0100

    13h40

--- static/js/sw.js
+++ static/js/sw.js
@@ -9,37 +9,37 @@
 //   console.log('Activated', event);
 // });
 
-self.addEventListener('push', function(event) {
-  console.log('[Service Worker] Push Received.');
-  console.log(event);
-  console.log(event.data.json());
-  let title = 'Server Push';
-  let options = {
-    body: 'push TEST',
-    icon: 'https://lx999.github.io/pocpwa/static/img/icons/favicon.ico',
-    data: {
-        link: 'https://lx999.github.io/pocpwa/#/'
-      }
-  };
-  if (event.data) {
-    options = event.data.json();
-    title = options.title;
-  }
-  event.waitUntil(self.registration.showNotification(title, options));
-});
+// self.addEventListener('push', function(event) {
+//   console.log('[Service Worker] Push Received.');
+//   console.log(event);
+//   console.log(event.data.json());
+//   let title = 'Server Push';
+//   let options = {
+//     body: 'push TEST',
+//     icon: 'https://lx999.github.io/pocpwa/static/img/icons/favicon.ico',
+//     data: {
+//         link: 'https://lx999.github.io/pocpwa/#/'
+//       }
+//   };
+//   if (event.data) {
+//     options = event.data.json();
+//     title = options.title;
+//   }
+//   event.waitUntil(self.registration.showNotification(title, options));
+// });
 
-self.addEventListener('notificationclick', event => {
-  const notification = event.notification;
-  const action = event.action;
-  const link = notification.data.link;
-  if (action !== 'close') {
-    if (link) {
-      clients.openWindow(link);
-    }
-  }
-  notification.close();
-  console.log('notificationclick action is', action);
-});
+// self.addEventListener('notificationclick', event => {
+//   const notification = event.notification;
+//   const action = event.action;
+//   const link = notification.data.link;
+//   if (action !== 'close') {
+//     if (link) {
+//       clients.openWindow(link);
+//     }
+//   }
+//   notification.close();
+//   console.log('notificationclick action is', action);
+// });
 
 // self.addEventListener('push', function(event) {
 //   console.log(event);
@@ -63,4 +63,34 @@ self.addEventListener('notificationclick', event => {
 //     }
 //   });
 // });   
+self.addEventListener('push', function(event) {
 
+  var apiPath = '<apiPath>';
+  event.waitUntil(registration.pushManager.getSubscription().then(function (subscription){
+
+      return fetch(apiPath).then(function(response){
+          if(response.status !== 200){
+              throw new Error();
+          }
+
+          return response.json().then(function(data){
+            console.log('data:' + data);
+              var title = data.title;
+              var message = data.body;
+              var icon = data.icon;
+              var tag = data.tag;
+              var url = data.url;
+              return self.registration.showNotification(title,{
+                 body: message,
+                 icon: icon,
+                 tag: tag,
+                 data: url
+              });
+          })
+      }).catch(function(err){
+
+      })
+
+  }));
+  return;
+});
