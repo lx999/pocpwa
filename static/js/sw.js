8a877e879e57bf977dcbcd7c229839b40bfc6b0c
FR01_AWL\A689415 <xiang.2.li@worldline.com>
2018-03-15 09:38:25 +0100

    09h38

--- static/js/sw.js
+++ static/js/sw.js
@@ -15,7 +15,7 @@ const messaging = firebase.messaging();
 messaging.setBackgroundMessageHandler(function(payload) {
   const title = 'Hello World';
   const options = {
-    body: payload.data.status;
+    body: payload.data.status
   };
   return self.registration.showNotification(title, options);
 })
